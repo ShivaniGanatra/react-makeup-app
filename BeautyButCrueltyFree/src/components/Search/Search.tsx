18554598d3b73e9fc523fd0a74cf6046beced1c8
ShivaniGanatra <shivaniganatra@live.com>
2024-09-05 17:59:53 +0100

    Created basic nav template with search input

---
+++ BeautyButCrueltyFree/src/components/Search/Search.tsx
@@ -0,0 +1,10 @@
+
+const Search = () => {
+  return (
+    <div>
+      <input value="search for makeup items" type="text" />
+    </div>
+  )
+}
+
+export default Search
--- BeautyButCrueltyFree/src/containers/Nav/Nav.tsx
+++ BeautyButCrueltyFree/src/containers/Nav/Nav.tsx
@@ -1,8 +1,14 @@
 
 import "./Nav.scss"
+import Search from "./../../components/Search/Search"
 const Nav = () => {
   return (
-    <nav className="nav">Nav</nav>
+    <nav className="nav">
+      <div className="nav__search">
+        <Search/>
+      </div>
+      <h1 className="nav__heading">Beauty but cruelty free</h1>
+    </nav>
   )
 }
 
