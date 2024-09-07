5a14e531347694794a21fe26e7ece5bcca648565
ShivaniGanatra <shivaniganatra@live.com>
2024-09-07 21:05:59 +0100

    Created a function to store values in checkilst section. I will try to pass these up to parent using functions and state

--- BeautyButCrueltyFree/src/components/ChecklistSection/ChecklistSection.tsx
+++ BeautyButCrueltyFree/src/components/ChecklistSection/ChecklistSection.tsx
@@ -1,18 +1,30 @@
+import { useState } from "react"
 import "./ChecklistSection.scss"
 
 interface ChecklistSectionProps {
   variant:string
 }
 
+
+
 const ChecklistSection: React.FC<ChecklistSectionProps>  = ({variant}) => {
+
+  const [value,setValue] = useState("")
+
+
+const storeValue = (productType:string):undefined => {
+  setValue(productType)
+  console.log(productType)
+}
+
   return (
     <section className="checklist">
         <h1 className="checklist__title">{variant}</h1>
             <section className="checklist__section">
                 <button className="checklist__section-title">face</button>
-                <button className="checklist__product-type">Foundation</button>
-                <button className="checklist__product-type">Blush</button>
-                <button className="checklist__product-type">Bronzer</button>
+                <button onClick={() => storeValue("foundation")} className="checklist__product-type">Foundation</button>
+                <button onClick={() => storeValue("blush")} className="checklist__product-type">Blush</button>
+                <button onClick={() => storeValue("bronzer")} className="checklist__product-type">Bronzer</button>
             </section> 
             <section className="checklist__section">
                 <h2 className="checklist__section-title">eyes</h2>
@@ -24,13 +36,17 @@ const ChecklistSection: React.FC<ChecklistSectionProps>  = ({variant}) => {
             </section>
             <section className="checklist__section">
                 <h2 className="checklist__section-title">lips</h2>
-                <button className="checklist__product-type">Lip-liner</button>
-                <button className="checklist__product-type">Lipstick need to specify category too</button>
-                <button className="checklist__product-type">Lip Gloss need to specify category too</button>
+                <button onClick={() => storeValue("liner")} className="checklist__product-type">Lip-liner</button>
+                <button onClick={() => storeValue("lip")} className="checklist__product-type">Lipstick need to specify category too</button>
+                <button onClick={() => storeValue("gloss")} className="checklist__product-type">Lip Gloss need to specify category too</button>
             </section>
     </section>
   )
 }
+
+
+//find a way to have state in prior one and chnage state here using function
+//the varaint which is the title cruelty free and vegan will also need to be used to choose the array to use
 
 export default ChecklistSection
 
